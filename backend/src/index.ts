cbf26685598331b4406fb11c6e60f8714bfdb045
Chetan Seervi <chetanseervi513@gmail.com>
2026-01-25 09:38:23 +0530

    add postgres database in compose.yaml

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,10 +1,13 @@
 import { Hono } from "hono";
 
-const app = new Hono();
+const app = new Hono()
+	.get("/", (c) => {
+		return c.json({ message: "Hello Hono!" });
+	})
 
-app.get("/", (c) => {
-	return c.text("Hello Hono!");
-});
+	.get("/health", (c) => {
+		return c.json({ status: "ok" });
+	});
 
 export default {
 	port: process.env.BACKEND_PORT ?? 3002,
